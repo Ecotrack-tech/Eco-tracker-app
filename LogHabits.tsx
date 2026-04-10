f25a5c103b540136c152ac84263031521cd43957
Ecotrack-tech <newlife.rebounce@gmail.com>
2026-04-10 19:52:44 +0200

    Create LogHabits component with basic layout

---
+++ LogHabits.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function LogHabits() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Log Habits Page</h1>
+      <p>This page is working ✅</p>
+    </div>
+  );
+}
